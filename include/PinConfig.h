10794eba7c533ab2085cd83365ce5d3dd6252a48
lyuboasenov <lyubo.asenov@gmail.com>
2022-11-23 15:39:37 +0200

    change encoder pins

--- include/PinConfig.h
+++ include/PinConfig.h
@@ -2,8 +2,8 @@
 #define PIN_CONFIG_H
 
 //**** CONNECTIONS *****
-#define ENCODER_A_PIN       2		//Interrupt pin required for Encoder for optimal response
-#define ENCODER_B_PIN       3		//Interrupt pin required for Encoder for optimal response
+#define ENCODER_A_PIN       3		//Interrupt pin required for Encoder for optimal response
+#define ENCODER_B_PIN       2		//Interrupt pin required for Encoder for optimal response
 // Define either TRIGGER_PIN or LED_PIN, as they share a pin
 //#define TRIGGER_PIN         4    	//Debugging purposes with scope
 //PIN 9~13: NRF24 RADIO
@@ -11,6 +11,6 @@
 #define MOSFET_100OHM_PIN   A3
 #define BUTTON_PIN          4    	// physical pin , use internal pullup
 #define CURRENT_PIN         A1
-#define ADC_PIN_NR           5     	// A5, Match to CURRENT_PIN for configuring registers ADC
+#define ADC_PIN_NR           1     	// A5, Match to CURRENT_PIN for configuring registers ADC
 
 #endif // PIN_CONFIG_H
